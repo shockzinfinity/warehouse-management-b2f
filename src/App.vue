19d9a7ade1d82dbb5020c3739bb4a838ca0b23b1
JunYu <shockzinfinity@gmail.com>
2020-07-10 22:16:29 +0900

    navigation drawer 설정

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
   v-app
     v-app-bar(app color="primary" dark)
-      v-app-bar-nav-icon
+      v-app-bar-nav-icon(@click="drawer = !drawer")
       v-toolbar-title Page title
       v-spacer
       v-btn(icon to="/about") <v-icon>mdi-magnify</v-icon>
+    v-navigation-drawer(app v-model="drawer")
+      v-list-item
+        v-list-item-content
+          v-list-item-title.title Application
+          v-list-item-subtitle subtext
     v-main
       router-view
     v-footer(app color="primary" dark absolute)
@@ -15,12 +20,11 @@
 <script>
 export default {
   name: 'App',
-
   components: {
   },
-
   data: () => ({
-    //
+    drawer: false,
+    items: []
   })
 }
 </script>
