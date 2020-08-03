604d999f1ae421a467552857c268c5f92b7b8547
Jun Yu <shockzinfinity@gmail.com>
2020-08-03 17:46:50 +0900

    pwa 1st

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
+  // pwa: {
+  //   themeColor: '#42b983',
+  //   msTileColor: '#42b983',
+  //   appleMobileWebAppCache: 'yes',
+  //   manifestOptions: {
+  //     background_color: '#42b983',
+  //   },
+  // },
 }
