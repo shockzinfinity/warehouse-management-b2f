0dabbff8f80c668f564c99a1111d21d4fdda3faa
Jun Yu <shockzinfinity@gmail.com>
2020-08-05 20:52:12 +0900

    manifest 통합

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,38 @@ module.exports = {
     appleMobileWebAppCache: 'yes',
     manifestOptions: {
       background_color: '#42b983',
+      start_url: '/?utm_source=homescreen',
+      display: 'standalone',
+      name: 'warehouse-management-b2f',
+      short_name: 'wmsPWA',
+      theme_color: '#42b983',
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/android-chrome-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: './img/icons/android-chrome-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    workboxOptions: {
+      offline: false,
     },
   },
 }
